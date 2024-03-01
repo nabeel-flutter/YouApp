8e2894cc49d0898921859c3874247510a78f0956
temoorx <temoorx@gmail.com>
2024-03-01 14:22:23 +0500

    Refactor HomeCarouselWidget to use StatefulWidget

--- lib/src/pages/home/components/home_crousel.dart
+++ lib/src/pages/home/components/home_crousel.dart
@@ -2,42 +2,78 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:new_beginnings/src/app/app_export.dart';
 
-class HomeCarouselWidget extends StatelessWidget {
-  const HomeCarouselWidget({super.key});
+class HomeCarouselWidget extends StatefulWidget {
+  const HomeCarouselWidget({Key? key}) : super(key: key);
+
+  @override
+  _HomeCarouselWidgetState createState() => _HomeCarouselWidgetState();
+}
+
+class _HomeCarouselWidgetState extends State<HomeCarouselWidget> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(23.0),
-      child: CarouselSlider(
-        options: CarouselOptions(
-          height: 200,
-          aspectRatio: 16 / 19,
-          viewportFraction: 1,
-          initialPage: 0,
-          enableInfiniteScroll: true,
-          reverse: false,
-          autoPlay: false,
-          autoPlayInterval: const Duration(seconds: 3),
-          autoPlayAnimationDuration: const Duration(milliseconds: 800),
-          autoPlayCurve: Curves.fastOutSlowIn,
-          enlargeCenterPage: false,
-          scrollDirection: Axis.horizontal,
-        ),
-        items: [
-          GestureDetector(
-            onTap: () {
-              debugPrint('Tapped on Image');
-            },
-            child: const Image(
-              image: AssetImage(AssetsConstants.crouselImage1),
+      child: Stack(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200,
+              aspectRatio: 16 / 19,
+              viewportFraction: 1,
+              initialPage: _currentIndex,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: false,
+              autoPlayInterval: const Duration(seconds: 3),
+              autoPlayAnimationDuration: const Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: false,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              scrollDirection: Axis.horizontal,
             ),
+            items: [
+              GestureDetector(
+                onTap: () {
+                  debugPrint('Tapped on Image');
+                },
+                child: const Image(
+                  image: AssetImage(AssetsConstants.crouselImage1),
+                ),
+              ),
+              const Image(
+                image: AssetImage(AssetsConstants.crouselImage1),
+              ),
+              const Image(
+                image: AssetImage(AssetsConstants.crouselImage1),
+              ),
+            ],
           ),
-          const Image(
-            image: AssetImage(AssetsConstants.crouselImage1),
-          ),
-          const Image(
-            image: AssetImage(AssetsConstants.crouselImage1),
+          Positioned(
+            bottom: 30,
+            right: 140,
+            child: DotsIndicator(
+              axis: Axis.horizontal,
+              decorator: DotsDecorator(
+                color: ColorConstants.white,
+                activeColor: ColorConstants.subTextColor,
+                activeSize: const Size(18, 8),
+                size: const Size(8, 8),
+                spacing: const EdgeInsets.symmetric(horizontal: 4),
+                activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              dotsCount: 3,
+              position: _currentIndex,
+              reversed: false,
+            ),
           ),
         ],
       ),
