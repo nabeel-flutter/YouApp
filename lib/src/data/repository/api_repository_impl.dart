aa46d1b54a68162352f8144efeeb5beb478a70fe
temoorx <temoorx@gmail.com>
2024-03-12 15:55:22 +0500

    Add import statement for UserDataModel

--- lib/src/data/repository/api_repository_impl.dart
+++ lib/src/data/repository/api_repository_impl.dart
@@ -12,6 +12,7 @@ import 'package:new_beginnings/src/domain/model/data_list.dart';
 import 'package:new_beginnings/src/domain/model/docotor.dart';
 import 'package:new_beginnings/src/domain/model/medical_records.dart';
 import 'package:new_beginnings/src/pages/appointment/models/appointments_details_dto.dart';
+import 'package:new_beginnings/src/pages/profile/model/userdata_model.dart';
 
 class ApiRepositoryImpl extends ApiRepository {
   final SoftTechTestApi softTechTestApi;
