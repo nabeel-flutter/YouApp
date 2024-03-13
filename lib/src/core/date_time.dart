c60e33d398e47432bdd07ab7f1b63e252e162344
temoorx <temoorx@gmail.com>
2024-03-13 15:58:39 +0500

    Add bookAppointment method to ApiRepository

--- lib/src/core/date_time.dart
+++ lib/src/core/date_time.dart
@@ -7,7 +7,8 @@ class DateTimeUtil {
 
   String timeStampToFormattedDate(int timeStamp) {
     try {
-      final DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(timeStamp * 1000).toUtc();
+      final DateTime dateTime =
+          DateTime.fromMillisecondsSinceEpoch(timeStamp * 1000).toUtc();
       String formattedDate = DateFormat('dd MMM hh:mm a').format(dateTime);
       return formattedDate;
     } catch (e) {
@@ -38,5 +39,24 @@ class DateTimeUtil {
     } catch (e) {
       return dateTime;
     }
+  }
+
+  String convertTo24HourFormat(String time) {
+    final List<String> parts = time.split(' ');
+    final List<String> timeParts = parts[0].split(':');
+    int hour = int.parse(timeParts[0]);
+    final int minute = int.parse(timeParts[1]);
+    final String period = parts[1];
+
+    if (period == 'PM' && hour != 12) {
+      hour += 12;
+    } else if (period == 'AM' && hour == 12) {
+      hour = 0;
+    }
+
+    final String hourStr = hour.toString().padLeft(2, '0');
+    final String minuteStr = minute.toString().padLeft(2, '0');
+
+    return '$hourStr:$minuteStr';
   }
 }
--- lib/src/pages/appointment/cubit/book_appointment/cubit/book_appointment_state.dart
+++ lib/src/pages/appointment/cubit/book_appointment/cubit/book_appointment_state.dart
@@ -3,8 +3,8 @@ part of 'book_appointment_cubit.dart';
 @freezed
 class BookAppointmentState with _$BookAppointmentState {
   const factory BookAppointmentState.initial() = _Initial;
-  const factory BookAppointmentState.loaded() = _Loaded;
+  const factory BookAppointmentState.loaded(BaseResponseDto<dynamic> data) = _Loaded;
   const factory BookAppointmentState.loading() = _Loading;
-  const factory BookAppointmentState.errorl() = _Error;
+  const factory BookAppointmentState.error(String message) = _Error;
   
 }
