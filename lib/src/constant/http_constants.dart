62b5591a9c9e1b6d5d58dd91dc391d923111c5fe
Muhammad Nabee <nabeelshakeel966@gmail.com>
2024-03-15 11:08:34 +0500

    bug fixing

--- lib/src/constant/http_constants.dart
+++ lib/src/constant/http_constants.dart
@@ -1,13 +1,14 @@
 class HttpConstants {
   static const bool isProduction = true;
-  static const String baseUrlArticle =
+  static const String ngroklink = 'https://95d8-2407-aa80-14-4f4-d1cf-425c-1994-9f5f.ngrok-free.app/api/v1/'
+;  static const String baseUrlArticle =
       'https://13be-111-88-87-28.ngrok-free.app/api/v1/';
   static const String base = isProduction
       ? "http://ec2-54-164-108-167.compute-1.amazonaws.com:7000/api/v1/"
-      : 'https://13be-111-88-87-28.ngrok-free.app/api/v1/';
+      : ngroklink;
   static const String urlBase = isProduction
       ? "http://ec2-54-164-108-167.compute-1.amazonaws.com:7000/api/v1/"
-      : 'https://13be-111-88-87-28.ngrok-free.app/api/v1/';
+      : ngroklink;
   static const String downloadRecipt = 'payment-receipt-download';
   static const String token = 'get/token';
   static const String login = 'login';
