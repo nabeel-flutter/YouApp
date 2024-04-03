a3581fc2d90f4698123aedec55de28a46557ee59
ayanappdev <130014983+ayanappdev@users.noreply.github.com>
2024-04-03 11:12:45 +0500

    my_logs.dart

--- lib/src/pages/my_logs/my_logs.dart
+++ lib/src/pages/my_logs/my_logs.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:new_beginnings/src/pages/my_logs/cubit/mylogs_cubit.dart';
+import 'package:new_beginnings/src/pages/my_logs/cubit/mylogs_state.dart';
 
 class MyLogs extends StatelessWidget {
   const MyLogs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('There is no appointment available yet'),
+    return BlocProvider(
+      create: (context) => LogCubit(),
+      child: BlocBuilder<LogCubit, LogState>(
+        builder: (context, state) {
+          if (state is DataLoading) {
+            return CircularProgressIndicator();
+          } else if (state is DataLoaded) {
+            return Text('Data: ${state.data}');
+          } else if (state is DataError) {
+            return Text('Error: ${state.message}');
+          }
+          return ElevatedButton(
+            onPressed: () => context.read<LogCubit>().fetchData(),
+            child: Text('Load Data'),
+          );
+        },
+      ),
     );
   }
 }
