bd6619345bc5aa301a7902978d2566cbb7972afd
ayanappdev <130014983+ayanappdev@users.noreply.github.com>
2024-04-05 15:57:14 +0500

    convert widget method into classes

--- lib/src/pages/my_logs/widgets/log_item_widgets.dart
+++ lib/src/pages/my_logs/widgets/log_item_widgets.dart
@@ -1,72 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:new_beginnings/src/pages/my_logs/model/my_logs_model.dart';
 
-Widget buildLogDataView(LogDetails? logDetails, BuildContext context) {
-  if (logDetails?.data == null || logDetails!.data!.isEmpty) {
-    return const Center(child: Text("No log data available"));
-  } else {
-    return ListView.builder(
-      itemCount: logDetails.data!.length,
-      itemBuilder: (context, index) {
-        return _buildLogItem(logDetails.data![index], context);
-      },
-    );
+class LogDataView extends StatelessWidget {
+  final LogDetails? logDetails;
+
+  const LogDataView({Key? key, this.logDetails}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (logDetails?.data == null || logDetails!.data!.isEmpty) {
+      return const Center(child: Text("No log data available"));
+    } else {
+      return ListView.builder(
+        itemCount: logDetails!.data!.length,
+        itemBuilder: (context, index) {
+          return LogItem(logData: logDetails!.data![index]);
+        },
+      );
+    }
   }
 }
 
-Widget _buildLogItem(LogData logData, BuildContext context) {
-  return Container(
+class LogItem extends StatelessWidget {
+  final LogData logData;
+
+  const LogItem({Key? key, required this.logData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
           const SizedBox(height: 15),
           Row(
             children: [
-              _buildGroupedFields(
-                'Appointment ID',
-                logData.id!.substring(0, 6),
-                'Time Slot',
-                logData.timeSlot!,
-                'Payment Type',
-                logData.paymentType!,
+              GroupedFields(
+                title1: 'Appointment ID',
+                content1: logData.id!.substring(0, 6),
+                title2: 'Time Slot',
+                content2: logData.timeSlot!,
+                title3: 'Payment Type',
+                content3: logData.paymentType!,
               ),
-              _buildGroupedFields(
-                'Date of Service (DOS)',
-                logData.appointmentDate!.substring(0, 10),
-                'Location',
-                logData.location!.substring(0, 11),
-                'Payment Status',
-                logData.paymentStatus!,
+              GroupedFields(
+                title1: 'Date of Service (DOS)',
+                content1: logData.appointmentDate!.substring(0, 10),
+                title2: 'Location',
+                content2: logData.location!.substring(0, 11),
+                title3: 'Payment Status',
+                content3: logData.paymentStatus!,
               ),
             ],
           ),
           const SizedBox(height: 12),
           const Divider(),
         ],
-      ));
+      ),
+    );
+  }
 }
 
-Widget _buildGroupedFields(String title1, String content1, String title2,
-    String content2, String title3, String content3) {
-  return Expanded(
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(title1),
-        Text(content1,
-            style: const TextStyle(
-                fontWeight: FontWeight.bold, color: Color(0xff023E3F))),
-        const SizedBox(height: 8.0),
-        Text(title2),
-        Text(content2,
-            style: const TextStyle(
-                fontWeight: FontWeight.bold, color: Color(0xff023E3F))),
-        const SizedBox(height: 8.0),
-        Text(title3),
-        Text(content3,
-            style: const TextStyle(
-                fontWeight: FontWeight.bold, color: Color(0xff023E3F))),
-      ],
-    ),
-  );
+class GroupedFields extends StatelessWidget {
+  final String title1;
+  final String content1;
+  final String title2;
+  final String content2;
+  final String title3;
+  final String content3;
+
+  const GroupedFields({
+    Key? key,
+    required this.title1,
+    required this.content1,
+    required this.title2,
+    required this.content2,
+    required this.title3,
+    required this.content3,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title1),
+          Text(content1,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, color: Color(0xff023E3F))),
+          const SizedBox(height: 8.0),
+          Text(title2),
+          Text(content2,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, color: Color(0xff023E3F))),
+          const SizedBox(height: 8.0),
+          Text(title3),
+          Text(content3,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, color: Color(0xff023E3F))),
+        ],
+      ),
+    );
+  }
 }
